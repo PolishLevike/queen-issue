3fa3a01dbab9b24f4f79d5daeeac38a7371605a6
rikss2 <kristofkiss3@gmail.com>
2023-04-14 09:17:29 +0200

    Project init

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+class state:
+
+class cella:
+    def királyno = False
+    ## ez a mező megmondja, hogy van e királyő a cellában
+
+class board:
+    ##sakktábla
+    def oszlop []
+    def cellák
